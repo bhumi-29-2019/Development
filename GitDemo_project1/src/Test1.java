8a7cf29446c76ae5713d18d0d245bea559fcd5e6
bhumi_meher_2909 <mebhumi29@gmail.com>
2020-01-02 00:11:20 +0530

    commit

---
+++ GitDemo_project1/src/Test1.java
@@ -0,0 +1,15 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String hungry="yes";
+		if(hungry=="yes") {
+			System.out.println("Yes i am hungry");
+			
+		}
+		System.out.println("no i am not hungry");
+		// TODO Auto-generated method stub
+
+	}
+
+}
